230f0cb72130de7dad3beb0c77986ee849974397
Sabari Anand <sabari.senthamaraikannan@syncfusion.com>
2021-08-09 22:33:29 +0530

    Added Maps sample for Vue 3.

---
+++ README.md
@@ -0,0 +1,7 @@
+The sample demonstrates how to integrate the Syncfusion Maps component into a Vue 3 application using the Vue CLI. The following features are shown in this sample.
+1. Binding GeoJSON data source.
+2. Introducing title.
+3. Applying color mapping.
+4. Enabling Legend for the color mapped shapes.
+5. Enabling data labels for the shapes.
+6. Enabling tooltip for the shapes.
---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <ejs-maps :titleSettings='titleSettings' :legendSettings='legendSettings'>
+        <e-layers>
+            <e-layer :shapeData='shapeData' :shapePropertyPath='shapePropertyPath' :shapeDataPath='shapeDataPath' :dataSource='dataSource' :shapeSettings='shapeSettings' :dataLabelSettings='dataLabelSettings' :tooltipSettings='tooltipSettings'></e-layer>
+        </e-layers>
+    </ejs-maps>
+</template>
+
+<script>
+import { MapsComponent, LayersDirective, LayerDirective, MapAjax, Legend, DataLabel, MapsTooltip } from '@syncfusion/ej2-vue-maps'
+
+export default {
+  name: 'App',
+  components: {
+    'ejs-maps': MapsComponent,
+    'e-layers': LayersDirective,
+    'e-layer': LayerDirective 
+  },
+  data() {
+    return {
+      titleSettings: {
+          text: 'UN security council countries'
+      },
+      shapeData: new MapAjax('https://cdn.syncfusion.com/maps/map-data/world-map.json'),
+      dataSource: [{  "Country": "China", "Membership": "Permanent"},
+            {"Country": "France","Membership": "Permanent" },
+            { "Country": "Russia","Membership": "Permanent"},
+            {"Country": "Kazakhstan","Membership": "Non-Permanent"},
+            { "Country": "Poland","Membership": "Non-Permanent"},
+            {"Country": "Sweden","Membership": "Non-Permanent"}],
+      shapePropertyPath: 'name',
+      shapeDataPath: 'Country',
+      shapeSettings: {
+            colorValuePath: 'Membership',
+            colorMapping: [
+                {
+                    value: 'Permanent', color: '#D84444'
+                },
+                {
+                    value: 'Non-Permanent', color: '#316DB5'
+                }
+            ]
+      },
+      dataLabelSettings: {
+            visible: true,
+            labelPath: 'name',
+            smartLabelMode: 'Trim'
+      },
+      legendSettings: {
+          visible: true
+      },
+      tooltipSettings: {
+          visible: true,
+          valuePath: 'Country'
+      }
+    }
+  },
+  provide: {
+    maps: [ Legend, DataLabel, MapsTooltip ]
+  }
+}
+</script>
